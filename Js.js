671ac36c508e27d28fd281110d1f9ba0af27b7e7
kioskeda <289049473+kioskedamed@users.noreply.github.com>
2026-06-27 10:14:12 -0500

    Primeros auxilios
    se han añadido cambios en la introuducción, importancia, ancción inmediata, presición y temporabilidad, intervención crucial, primeros 4 párafos del libro primros auxilios con el fin de ayudar a estudiantes de medicina. 
    ATT: José fernando cano peláez.

---
+++ Js.js
@@ -0,0 +1,5 @@
+//Introducción primeros auxilios:
+a=["Los primeros auxilios son la asistencia inmediata, pricsa y temporal que se brinda a personas que han sufrido accidentes, enfermedades o lecciones en combate, antes de recibir ateción médica profesional en un centro asistencial. No se considera un tratamiento médico en sí mismo, si no un conjunto de desiciones tomadas con sensates para optimizar las condiciones del afectado hasta que sea atendido por un mpedico o trasladado a un centro de salud cercano."];
+
+//Importancia de los primeros auxilios:
+b=["Acción inmediata: La rápidez en la aplicación de los primeros auxilios puede ser crucial para la supervivencia o la recuperación del indiviudo.", "Presición y temporabilidad: La asistencia debe ser precisa y temporal, enfocándose en las mediddas necesarias hasta que se pueda recibir atención médica profesional.","Intervención crucial: Los primeros auxilios no remplazan la atención mécdica, pero son una intervención crucial que puede meejorar significativamente las posinilidades de recuperación del afectado."];
