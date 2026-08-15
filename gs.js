bbb2227c9af3f3f43c028fb4e4fee46a10014106
kioskeda <289049473+kioskedainfo@users.noreply.github.com>
2026-08-15 13:25:22 -0500

    Add onload function to log loading records

---
+++ gs.js
@@ -0,0 +1,8 @@
+onload salu(){
+    let num = 0;
+    const num1 = 1;
+    while num <= 1000000 {
+        num = num + num1;
+        console.log("Van " + num1 + "registros de carga del libro.");
+    }
+}
